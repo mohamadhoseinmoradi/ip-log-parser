5122c2965006520de050ec046ef205243845e46f
Mohamad Hosein Moradi <72094509+mohamadhoseinmoradi@users.noreply.github.com>
2021-11-28 21:59:06 +0100

    Update ip-log-parser.py

--- ip-log-parser.py
+++ ip-log-parser.py
@@ -4,9 +4,8 @@ with open("ip.log", "r") as file:
     for file in file:
         ip = file.split(" ")[0]
         if ip not in ip_dict:
-            ip_dict[ip] = 1
-        else:
-            ip_dict[ip] += 1
+            ip_dict[ip] = 0
+        ip_dict[ip] += 1
 
 new_ip_dict = dict(sorted(ip_dict.items(), key= lambda item: item[1], reverse=True)[:5])
 #print (*new_ip_dict.items(), sep="\n")
