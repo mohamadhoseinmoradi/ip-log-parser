58d535bd1678175425a0027a2676847f619a21aa
Mohamad Hosein Moradi <72094509+mohamadhoseinmoradi@users.noreply.github.com>
2022-01-16 15:00:39 +0100

    Update ip-log-parser.py

--- ip-log-parser.py
+++ ip-log-parser.py
@@ -1,13 +1,12 @@
 ip_dict = {}
 
 with open("ip.log", "r") as file:
-    for file in file:
-        ip = file.split(" ")[0]
+    for line in file:
+        ip = line.split(" ")[0]
         if ip not in ip_dict:
             ip_dict[ip] = 0
         ip_dict[ip] += 1
 
-new_ip_dict = dict(sorted(ip_dict.items(), key= lambda item: item[1], reverse=True)[:5])
-#print (*new_ip_dict.items(), sep="\n")
-for i,j in new_ip_dict.items():
+ip_dict_top_5 = dict(sorted(ip_dict.items(), key=lambda item: item[1], reverse=True)[:5])
+for i,j in ip_dict_top_5.items():
     print (f"occurence for the ip address of", i, "is: ", j)
